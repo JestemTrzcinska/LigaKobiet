8ab75593cfc17962d4ed7db0e7aaaefd1c3391f2
Patrycja <patrycja.trzcinska125@gmail.com>
2020-01-04 12:53:57 +0100

    do 39

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
@@ -18,7 +21,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('wrong');
+      setAlert('Hasła nie są takie same', 'danger');
     } else {
       console.log('SUCCESS');
     }
@@ -89,7 +92,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
 
 //else axios needed
 //   const newUser = {

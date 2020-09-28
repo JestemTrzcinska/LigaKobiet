9ef210d6fa6dfb43fc603ffd807c9d7718b797d9
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-09-28 18:42:14 +0200

    .

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,8 +9,6 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -20,6 +19,16 @@ app.use('/api/game', require('./routes/api/game'));
 app.use('/api/club', require('./routes/api/club'));
 app.use('/api/player', require('./routes/api/player'));
 app.use('/api/playerInClub', require('./routes/api/playerInClub'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 

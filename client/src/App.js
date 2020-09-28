2d1ba19fa80af2aa5861bd68da59acdf3090e5ec
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-09-28 20:38:41 +0200

    because heroku didnt work

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import PageNotFound from './components/layout/PageNotFound';
 import LoginRegister from './components/auth/LoginRegister';
-import Schedule from './components/schedule/Schedule';
+// import Schedule from './components/schedule/Schedule';
 import Dashboard from './components/schedule/Dashboard';
 import News from './components/news/News';
 
@@ -66,7 +66,7 @@ const App = () => {
           <Switch>
             <Route exact path='/' component={Landing} />
             <Route exact path='/loginregister' component={LoginRegister} />
-            <Route exact path='/schedule' component={Schedule} />
+            {/* <Route exact path='/schedule' component={Schedule} /> */}
             <Route exact path='/news' component={News} />
 
             <Route exact path='/game/:id' component={Game} />
